b051ad1d5ddb31145b56e0064ecc4990ae92bcbe
Mazen Ramadan <mazenramadan00@gmail.com>
2022-02-27 23:53:31 +0200

    Dynamic URLs

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index),
-    path('meetups/<slug:meetup_slug>', views.meetup_details),
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),
 ]
